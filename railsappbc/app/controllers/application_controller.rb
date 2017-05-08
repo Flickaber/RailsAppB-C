f64813c9cdc2a5c0150e1ef3b5051206bff30222
LauraBernard <laura@Flickaber>
2017-05-08 20:15:30 +0200

    Creation of the two statics english and french versions of the website with I8n gem of Rails.

--- railsappbc/app/controllers/application_controller.rb
+++ railsappbc/app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+    private
+    def set_locale
+      I18n.locale = extract_locale || I18n.default_locale
+    end
+    def extract_locale
+    parsed_locale = request.host.split('.').last
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+    end
+
 end
